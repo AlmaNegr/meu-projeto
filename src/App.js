f124f4f0a033f174abfad5cdbd5b7e5accf3e36e
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-06-26 17:21:55 -0300

    props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css'
 
-import React from 'react';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Teste from './components/Teste';
+import Pessoa from './components/Pessoa';
 
-function app() {
 
+function App() {
   return (
-    <div>
+    <div className='App'>
       <h1>Hello Alisson!</h1>
+      <HelloWorld />
+      <SayMyName nome="Alisson" />
+      <SayMyName nome="João" />
+      <Teste nome="Alhinho" />
+      <Pessoa
+      nome="Rodrigo"
+      idade="28"
+      profissao="Desenvolvedo Web"
+      foto="https://via.placeholder.com/150"
+      />
     </div>
   )
 }
 
-export default app
-
+export default App
---
+++ src/components/Teste.js
@@ -0,0 +1,10 @@
+function Teste(props) {
+
+    return (
+        <div>
+            <p>Pode demorar, mas eu {props.nome} vou conseguir</p>
+        </div>
+    )
+}
+
+export default Teste
