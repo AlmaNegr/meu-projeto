7c3dbfdda09743d41feb33c0b34a35cced9c03fd
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-07-13 17:32:26 -0300

    react icon

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./components1/pages/Home"
-import Contato from "./components1/pages/Contato"
-import Sobre from "./components1/pages/Sobre"
-import Navbar from "./components1/pages/Navbar"
+import Footer from "./components/layout/Footer"
 
 function App() {
 
   return(
     <div>
-      <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contato" element={<Contato />} />
-        <Route path="/sobre" element={<Sobre />} />
-      </Routes>
-      </BrowserRouter>
+      <Footer />
     </div>
   
   )
